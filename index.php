a6a24a4406fa084fb69704475f435534dc060d69
Kento K <33736354+KentoKana@users.noreply.github.com>
2019-01-25 23:24:30 -0500

    added index file (splash page), migrated td.html to td.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>TD</title>
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="stylesheets/css/index.css">
+  <style>@import url('https://fonts.googleapis.com/css?family=VT323');</style>
+</head>
+<body>
+  <main id="index">
+    <div class="titleDiv" id="titleDiv">
+      <div>
+        <h1 class="titleDiv__title">DEFENDING HOME</h1>    
+        <a href="td.php"><button class="titleDiv__button" id="titleDiv__button">Defend Your Home</button></a>
+      </div>
+    </div>
+  </main>
+</body>
+</html>
---
+++ td.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8" />
+  <title>TD</title>
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" type="text/css" href="td.css">
+</head>
+<body>
+  <main id="main">
+    <div id="test"></div>
+    <div class="page-wrapper">
+      <div id="tower-select">
+        <form name="user_menu">
+          <label for="nothing">Nothing</label>
+          <input type="radio" id="nothing" name="tower_select" value="0" checked />
+          <label for="basic-tower">Basic</label>
+          <input type="radio" id="basic-tower" name="tower_select" value="1" />
+          <label for="strong-tower">So Stronk</label>
+          <input type="radio" id="strong-tower" name="tower_select" value="2" />
+        </form>
+            <?php
+                if (isset($genderError)) {
+                    echo $genderError;
+                }
+            ?>
+        </span>
+      </div>
+      <canvas id="game-canvas" width="1000" height="600">
+        Your browser does not support the HTML5 canvas tag.
+      </canvas>
+    </div>
+  </main>
+  
+  <script src="td.js" ></script>
+</body>
+</html>
