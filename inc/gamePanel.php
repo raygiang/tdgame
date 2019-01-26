3c6e13a7519b8d83d749cd27bd3a1efeb8be3021
Beverly Li <beverly.li@live.com>
2019-01-26 15:00:28 -0500

    updated tower radio buttons

--- inc/gamePanel.php
+++ inc/gamePanel.php
@@ -1,21 +1,19 @@
  <form id="game-panel" name="game_panel">
   <h2>Game Panel</h2>
   <div id="tower-select">
-    <h3>Towers</h3>
-    <label for="nothing">Nothing</label>
-    <input type="radio" id="nothing" name="tower_select" value="0" checked />
-    <br>
-    <label for="arrow-tower">Arrow Tower</label>
-    <input type="radio" id="arrow-tower" name="tower_select" value="1" />
-    <br>
-    <label for="laser-tower">Laser Tower</label>
-    <input type="radio" id="laser-tower" name="tower_select" value="2" />
-    <br>
-    <label for="artillery-tower">Artillery Tower</label>
-    <input type="radio" id="artillery-tower" name="tower_select" value="3" />
-    <br>
-    <label for="cannon-tower">Cannon Tower</label>
-    <input type="radio" id="cannon-tower" name="tower_select" value="4" />
+	<h3>Towers</h3>
+	<label for="arrow-tower"><img class="tower_img" src="images/siegeBallista.png" alt="Arrow Tower"/></label>
+	<input type="radio" id="arrow-tower" name="tower_select" value="1" />
+	<br />
+	<label for="laser-tower"><img class="tower_img" src="images/towerDefense__laser.png" alt="Laser Tower" /></label>
+	<input type="radio" id="laser-tower" name="tower_select" value="2" />
+	<br />
+	<label for="artillery-tower"><img class="tower_img" src="images/towerDefense_artillery.png" alt="Artillery Tower"/></label>
+	<input type="radio" id="artillery-tower" name="tower_select" value="3" />
+	<br />
+	<label for="cannon-tower"><img class="tower_img" src="images/towerDefense_cannon.png" alt="Canon Tower"/></label>
+	<input type="radio" id="cannon-tower" name="tower_select" value="4" />
+	<br />
     <button id="upgrade" type='button'>Upgrade Tower</button>
   </div>
   <div id="items">
