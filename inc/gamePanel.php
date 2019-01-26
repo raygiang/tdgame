de2f70b1ea2ed10f681a100c7d7b8071a9a58a77
Kento K <33736354+KentoKana@users.noreply.github.com>
2019-01-26 18:21:46 -0500

    added inventory box

--- inc/gamePanel.php
+++ inc/gamePanel.php
@@ -1,22 +1,36 @@
  <form id="game-panel" name="game_panel">
-  <h2>Game Panel</h2>
-  <div id="tower-select">
-	<h3>Towers</h3>
-	<label for="arrow-tower"><img class="tower_img" src="images/siegeBallista.png" alt="Arrow Tower"/></label>
-	<input type="radio" id="arrow-tower" name="tower_select" value="1" />
-	<br />
-	<label for="laser-tower"><img class="tower_img" src="images/towerDefense__laser.png" alt="Laser Tower" /></label>
-	<input type="radio" id="laser-tower" name="tower_select" value="2" />
-	<br />
-	<label for="artillery-tower"><img class="tower_img" src="images/towerDefense_artillery.png" alt="Artillery Tower"/></label>
-	<input type="radio" id="artillery-tower" name="tower_select" value="3" />
-	<br />
-	<label for="cannon-tower"><img class="tower_img" src="images/towerDefense_cannon.png" alt="Canon Tower"/></label>
-	<input type="radio" id="cannon-tower" name="tower_select" value="4" />
-	<br />
-    <button id="upgrade" type='button'>Upgrade Tower</button>
-  </div>
-  <div id="items">
-    <h3>Your Items</h3>
-  </div>
-</form>
+ 	<h2>Game Panel</h2>
+ 	<div id="tower-select">
+ 		<h3>Towers</h3>
+ 		<label for="arrow-tower"><img class="tower_img" src="images/siegeBallista.png" alt="Arrow Tower"/></label>
+ 		<input type="radio" id="arrow-tower" name="tower_select" value="1" />
+ 		<br />
+ 		<label for="laser-tower"><img class="tower_img" src="images/towerDefense__laser.png" alt="Laser Tower" /></label>
+ 		<input type="radio" id="laser-tower" name="tower_select" value="2" />
+ 		<br />
+ 		<label for="artillery-tower"><img class="tower_img" src="images/towerDefense_artillery.png" alt="Artillery Tower"/></label>
+ 		<input type="radio" id="artillery-tower" name="tower_select" value="3" />
+ 		<br />
+ 		<label for="cannon-tower"><img class="tower_img" src="images/towerDefense_cannon.png" alt="Canon Tower"/></label>
+ 		<input type="radio" id="cannon-tower" name="tower_select" value="4" />
+ 		<br />
+ 		<button id="upgrade" type='button'>Upgrade Tower</button>
+ 	</div>
+ 	<div id="items">
+ 		<h3>Your Items</h3>
+ 		<table>
+ 			<tbody>
+ 				<tr>
+ 					<td><button type="button" class="items"></button></td>
+ 					<td><button type="button" class="items"></button></td>
+ 					<td><button type="button" class="items"></button></td>
+ 				</tr>
+ 				<tr>
+ 					<td><button type="button" class="items"></button></td>
+ 					<td><button type="button" class="items"></button></td>
+ 					<td><button type="button" class="items"></button></td>
+ 				</tr>
+ 			</tbody>
+ 		</table>
+ 	</div>
+ </form>
