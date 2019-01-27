9942493807269b6c685a12f06c7fb00132d95ae3
Kento K <33736354+KentoKana@users.noreply.github.com>
2019-01-27 14:16:01 -0500

    changed insert db logic

--- inc/submitScoreModal.php
+++ inc/submitScoreModal.php
@@ -9,29 +9,6 @@
               <input id="displayScore" value="" name="finalScore" disabled>
               <button type="button" id="restart">Back To Main Page</button>
               <button type="submit" class="formDiv__button" id="submit-score" name="submitScore">Submit Score</button></a>
-              <?php
-              if(isset($_POST["submitScore"]))
-              {
-                $insertStmt = $conn->prepare("INSERT INTO scoreboard VALUES (DEFAULT, :score, :player_id)");
-                $score = $_POST['finalScore'];
-                $playerId = $_REQUEST['player_id'];
-                filter_var($score, FILTER_SANITIZE_SPECIAL_CHARS);
-                filter_var($playerId, FILTER_SANITIZE_SPECIAL_CHARS);
-                $insertStmt->bindParam(':score', $score);
-                $insertStmt->bindParam(':player_id', $playerId);
-                try 
-                {
-                  $insertStmt->execute();
-                  $currentPlayerId = $conn->lastInsertId();
-                  header("Location: scoreboard.php?status=success");
-                  $_POST = array();
-                } 
-                catch(PDOException $e) 
-                {
-                  echo 'PDOException: ' . $e->getMessage();
-                }                  
-              }     
-              ?>
             </form>
           </div>
         </div>
@@ -40,3 +17,30 @@
   </div>
 </div>
 
+<?php
+if(isset($_POST["submitScore"]))
+{
+  $conn->prepare("INSERT INTO player VALUES (NULL, :player_name, :score)");
+  $score = $_POST['finalScore'];
+  $player = $_REQUEST['player'];
+
+                //Sanitize input
+  filter_var($score, FILTER_SANITIZE_SPECIAL_CHARS);
+  filter_var($player, FILTER_SANITIZE_SPECIAL_CHARS);
+
+                //Bind query parameters
+  $insertStmt->bindParam(':score', $score);
+  $insertStmt->bindParam(':player_name', $player);
+  try 
+  {
+    $insertStmt->execute();
+    $currentPlayerId = $conn->lastInsertId();
+    header("Location: scoreboard.php?status=success");
+    $_POST = array();
+  } 
+  catch(PDOException $e) 
+  {
+    echo 'PDOException: ' . $e->getMessage();
+  }                  
+}     
+?>
