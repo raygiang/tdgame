87380c3a4cdde3ac4cfa7c56b09c88a124bd031c
Kento K <33736354+KentoKana@users.noreply.github.com>
2019-01-27 13:56:30 -0500

    indentation

--- inc/backToScreen.php
+++ inc/backToScreen.php
@@ -1,17 +1,17 @@
-    <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-body">
-            <form action="index.php" method="POST">
-              <div class="formDiv" id="formDiv">
-                <div>
-                  <h2>Are you a quitter?</h2>
-                  <button type="button" class="button btn-secondary" data-dismiss="modal">Nope. Continue Game.</button>
-                  <a href="index.php"><button type="button" class="formDiv__button" id="formDiv__button" name="submit_button">Yes. Take me back.</button></a>
-                </div>
-              </div>
+<div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-body">
+        <form action="index.php" method="POST">
+          <div class="formDiv" id="formDiv">
+            <div>
+              <h2>Are you a quitter?</h2>
+              <button type="button" class="button btn-secondary" data-dismiss="modal">Nope. Continue Game.</button>
+              <a href="index.php"><button type="button" class="formDiv__button" id="formDiv__button" name="submit_button">Yes. Take me back.</button></a>
             </div>
-          </form>      
+          </div>
         </div>
-      </div>
+      </form>      
     </div>
+  </div>
+</div>
